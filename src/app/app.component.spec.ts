e97df1fae8f152639c963b160fd16682d11e7e9e
Jon Friskics <jonfriskics@gmail.com>
2017-05-15 16:36:04 -0400

    add SampleComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,62 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { SampleComponent } from './sample/sample.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        SampleComponent
       ],
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
+  it(`should contain the app-sample element @does-contain-element`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    const childNodes = fixture.debugElement.childNodes;
+
+    let sampleFound = 0;
+    childNodes.forEach(element => {
+      if (element.nativeNode.nodeType == 1) {
+        if (element.nativeNode.localName == 'app-sample') {
+          sampleFound = sampleFound + 1;
+        }
+      }
+    });
+
+    expect(sampleFound).toBe(1);
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should not contain elements other than app-sample @does-not-contain-other-elements`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    const childNodes = fixture.debugElement.childNodes;
+
+    let otherElementsFound = 0;
+    childNodes.forEach(element => {
+      if (element.nativeNode.nodeType == 1) {
+        if (element.nativeNode.localName == 'app-sample') {
+          // do nothing
+        } else {
+          otherElementsFound = otherElementsFound + 1;
+        }
+      }
+    });
+
+    expect(otherElementsFound).toBe(0);
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it(`should not contain any text nodes @does-not-contain-text-nodes`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    const childNodes = fixture.debugElement.childNodes;
+
+    let otherElementsFound = 0;
+    childNodes.forEach(element => {
+      expect(element.nativeNode.nodeType).not.toBe(3);
+    });
+
+    expect(otherElementsFound).toBe(0);
   }));
+
 });
